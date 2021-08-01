4b8637db0ea375bf73e18014aaf9f12fa85de033
ollimeah <olli76765@gmail.com>
2021-08-01 10:48:11 +0100

    Fix title in dashboard

--- staff/forms.py
+++ staff/forms.py
@@ -13,15 +13,15 @@ class SettingsForm(forms.Form):
     primary_colour = forms.CharField(label="Primary Colour", max_length=7, widget=forms.TextInput(attrs={'type': 'color'}))
     secondary_colour = forms.CharField(label="Secondary Colour", max_length=7, widget=forms.TextInput(attrs={'type': 'color'}))
 
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = '__all__'
+# class ProductForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
 
-class CategoryForm(forms.ModelForm):
-    class Meta:
-        model = Category
-        fields = '__all__'
+# class CategoryForm(forms.ModelForm):
+#     class Meta:
+#         model = Category
+#         fields = '__all__'
 
 class CategoryProductForm(forms.Form):
     products = forms.ModelMultipleChoiceField(Product.objects, widget = forms.CheckboxSelectMultiple)
@@ -31,10 +31,10 @@ class ProductManagementForm(forms.Form):
     # all = forms.BooleanField(label = "All Products", required = False)
     products = forms.ModelMultipleChoiceField(Product.objects, widget = forms.CheckboxSelectMultiple)
 
-class CollectionForm(forms.ModelForm):
-    class Meta:
-        model = Collection
-        fields = '__all__'
+# class CollectionForm(forms.ModelForm):
+#     class Meta:
+#         model = Collection
+#         fields = '__all__'
 
 class CollectionProductForm(forms.Form):
     products = forms.ModelMultipleChoiceField(Product.objects, widget = forms.CheckboxSelectMultiple)
