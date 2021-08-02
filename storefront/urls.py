ae1451a45775439cc51991db939cdc0e51e6e268
Olli Meah <olli76765@gmail.com>
2021-08-02 18:39:06 +0100

    Create category filters

--- storefront/urls.py
+++ storefront/urls.py
@@ -4,5 +4,6 @@ from storefront.views import *
 urlpatterns = [
     path('', home, name='home'),
     path('shop/', ProductListView.as_view(), name='shop'),
-    path('shop/<str:name>/', ProductDetailView.as_view(), name='product')
+    path('shop/<category>/', ProductListView.as_view(), name='shop-category'),
+    path('shop/product/<str:name>/', ProductDetailView.as_view(), name='product')
 ]
--- storefront/views.py
+++ storefront/views.py
@@ -1,25 +1,31 @@
-from django.shortcuts import render
-from staff.models import Product
+from django.shortcuts import get_object_or_404, render
+from staff.models import Category, Product
 from django.views import generic
 
 def home(request):
     return render(request, 'storefront/home.html', {})
-
-# def shop(request):
-#     available = Product.get_available()
-#     unavailable = Product.get_unavailable()
-#     context = {'available' : available, 'unavailable' : unavailable}
-#     return render(request, 'storefront/shop.html', context)
 
 class ProductListView(generic.ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'storefront/shop.html'
 
+    def get_queryset(self):
+        if 'category' in self.kwargs:
+            category = get_object_or_404(Category, name=self.kwargs['category'])
+            return Product.objects.filter(hidden=False, category=category)
+        else:
+            return Product.objects.filter(hidden=False)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['available'] = Product.get_available()
-        context['unavailable'] = Product.get_unavailable()
+        products = self.get_queryset()
+        context['available'] = products.filter(available=True)
+        context['unavailable'] = products.filter(available=False)
+        if 'category' in self.kwargs:
+            context['title'] = self.kwargs['category']
+        else:
+            context['title'] = "All Products"
         return context
 
 class ProductDetailView(generic.DetailView):
