f3d3a0dcb90dff8b2ee83a500d4787a27c36953d
Olli Meah <olli76765@gmail.com>
2021-08-03 20:16:07 +0100

    Create basket model

--- storefront/models.py
+++ storefront/models.py
@@ -1,3 +1,12 @@
+from staff.models import Collection, Product
 from django.db import models
+import uuid
 
-# Create your models here.
+class Device(models.Model):
+    device = models.UUIDField(default=uuid.uuid4)
+
+class Basket(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    date_updated = models.DateTimeField(auto_now=True)
+    products = models.ManyToManyField(Product)
+    collections = models.ManyToManyField(Collection)
