03a6ea78848a8ffbcb269905ac8c5dba28a3b164
ollimeah <olli76765@gmail.com>
2021-08-04 22:32:03 +0100

    Add intermediary model for basket items

--- storefront/models.py
+++ storefront/models.py
@@ -13,11 +13,23 @@ class Device(models.Model):
 class Basket(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
     date_updated = models.DateTimeField(auto_now=True)
-    products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through='BasketProduct')
     collections = models.ManyToManyField(Collection)
 
     @classmethod
     def add_product_to_basket(basket, device_code, product, quantity):
         device = Device.objects.get(code=device_code)
         basket, created = Basket.objects.get_or_create(device=device)
-        basket.products.add(product)
+        bp, created = BasketProduct.objects.get_or_create(basket=basket, product=product)
+        bp.add_quantity(quantity)
+        print(basket.products.all())
+        print(bp.quantity)
+
+class BasketProduct(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+    def add_quantity(self, quantity):
+        self.quantity = self.quantity + quantity
+        self.save()
