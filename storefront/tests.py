0afc940134f71fc61b787a93510077f17c0aa509
Olli Meah <olli76765@gmail.com>
2021-08-21 15:56:24 +0100

    Test device model

--- storefront/tests.py
+++ storefront/tests.py
@@ -1,3 +1,12 @@
+from storefront.models import Device
 from django.test import TestCase
+import uuid
 
 # Create your tests here.
+class DeviceTest(TestCase):
+
+    def test_new_device_id(self):
+        num_devices = len(Device.objects.all())
+        code = Device.get_new_device_id()
+        self.assertTrue(isinstance(code, uuid.UUID))
+        self.assertEqual(num_devices + 1, len(Device.objects.all()))
