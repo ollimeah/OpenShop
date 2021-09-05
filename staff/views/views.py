e7dd9905ecb201b8dcddd182a70b9620296cc872
Olli Meah <olli76765@gmail.com>
2021-09-05 11:35:56 +0100

    Add authentication to dashboard and settings

--- staff/views/views.py
+++ staff/views/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from staff.forms import SettingsForm
 from staff.models import Order, Product, Settings
+from django.contrib.auth.decorators import user_passes_test
 
+def staff_check(user):
+    return user.groups.filter(name='Staff').exists()
+
+@user_passes_test(staff_check, login_url='staff-login')
 def dashboard(request):
     context = {'orders_today' : Order.num_orders_today(), 'sales_today' : Order.sales_today(), 
                 'available_products' : Product.num_available(), 'best_sellers' : Product.best_sellers(5), 
                 'recent_orders' : Order.recent_orders(10)}
     return render(request, 'staff/dashboard.html', context)
 
+@user_passes_test(staff_check, login_url='staff-login')
 def settings(request):
     settings = Settings()
     if request.method == 'POST':
