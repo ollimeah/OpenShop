07108ff9ce63dcc869f70c16ea1932b511241296
ollimeah <olli76765@gmail.com>
2021-09-06 16:43:34 +0100

    Update and test FAQ pages

--- staff/views/store.py
+++ staff/views/store.py
@@ -1,31 +1,39 @@
 from staff.models import FAQ, Promotion, Delivery, CarouselImage
 from django.views import generic
 from django.urls import reverse
+from django.contrib.auth.mixins import UserPassesTestMixin
 
-class FAQListView(generic.ListView):
+class StaffTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
+    
+    def get_login_url(self):
+        return reverse('staff-login')
+
+class FAQListView(StaffTestMixin, generic.ListView):
     model = FAQ
     context_object_name = 'faqs'
     template_name = 'faqs/index.html'
 
-class FAQCreateView(generic.edit.CreateView):
+class FAQCreateView(StaffTestMixin, generic.edit.CreateView):
     model = FAQ
     fields = '__all__'
     template_name = 'faqs/new.html'
 
     def get_success_url(self):
-        return reverse('staff-faq', kwargs={'pk' : self.object.id})
+        return reverse('staff-faqs')
 
-class FAQUpdateView(generic.UpdateView):
+class FAQUpdateView(StaffTestMixin, generic.UpdateView):
     model = FAQ
     fields = '__all__'
     template_name = 'faqs/update.html'
 
     def get_success_url(self):
-        return reverse('staff-faq', kwargs={'pk' : self.object.id})
+        return reverse('staff-faqs')
 
-class FAQDeleteView(generic.DeleteView):
+class FAQDeleteView(StaffTestMixin, generic.DeleteView):
     model = FAQ
-    template_name = 'faqs/delete.html'
+    template_name = 'faqs/index.html'
 
     def get_success_url(self):
         return reverse('staff-faqs')
--- storefront/tests/tests_views.py
+++ storefront/tests/tests_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User, Group
+from staff.models import FAQ
 
 class LoginTest(TestCase):
 
@@ -62,3 +63,97 @@ class DashboardTest(TestCase):
         response = self.client.get(reverse('staff-dashboard'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'staff/dashboard.html')
+
+class FAQTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        username = 'staff'
+        password = 'test_password'
+        staff = User.objects.create_user(username=username, password=password)
+        staff.save()
+        group = Group.objects.create(name='Staff')
+        group.user_set.add(staff)
+        FAQ.objects.create(question="Test", answer="Test")
+    
+    def login_staff(self):
+        return self.client.login(username='staff', password='test_password')
+    
+    def test_index_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('staff-faqs'))
+        self.assertRedirects(response, '/staff/?next=/staff/faqs/')
+    
+    def test_index_url_exists_at_desired_location(self):
+        self.login_staff()
+        response = self.client.get('/staff/faqs/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_url_accessible_by_name(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faqs'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_uses_correct_template(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faqs'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/index.html')
+    
+    def test_new_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('staff-faqs-new'))
+        self.assertRedirects(response, '/staff/?next=/staff/faqs/new/')
+    
+    def test_new_url_exists_at_desired_location(self):
+        self.login_staff()
+        response = self.client.get('/staff/faqs/new/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_url_accessible_by_name(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faqs-new'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_uses_correct_template(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faqs-new'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/new.html')
+    
+    def test_update_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('staff-faq-update', kwargs={'pk':1}))
+        self.assertRedirects(response, '/staff/?next=/staff/faq/1/update/')
+    
+    def test_update_url_exists_at_desired_location(self):
+        self.login_staff()
+        response = self.client.get('/staff/faq/1/update/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_url_accessible_by_name(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faq-update', kwargs={'pk':1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_update_uses_correct_template(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faq-update', kwargs={'pk':1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/update.html')
+    
+    def test_delete_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('staff-faq-delete', kwargs={'pk':1}))
+        self.assertRedirects(response, '/staff/?next=/staff/faq/1/delete/')
+    
+    def test_delete_url_exists_at_desired_location(self):
+        self.login_staff()
+        response = self.client.get('/staff/faq/1/delete/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_url_accessible_by_name(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faq-delete', kwargs={'pk':1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_uses_correct_template(self):
+        self.login_staff()
+        response = self.client.get(reverse('staff-faq-delete', kwargs={'pk':1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'faqs/index.html')
